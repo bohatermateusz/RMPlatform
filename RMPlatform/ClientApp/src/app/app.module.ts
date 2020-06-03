0c8ad6fd5b65268a0cbbeaa389f6f0cad3407304
bohatermateusz <bohatermateusz@gmail.com>
2020-06-03 12:07:19 +0200

    Added random form component

--- RMPlatform/ClientApp/src/app/app.module.ts
+++ RMPlatform/ClientApp/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    FormComponent,
   ],
   imports: [
     BrowserModule,
