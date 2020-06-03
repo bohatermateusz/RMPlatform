77330af1039c13aebc9cc73c29b649439b52cb55
bohatermateusz <bohatermateusz@gmail.com>
2020-06-03 21:42:50 +0200

    added new component and installed material

--- RMPlatform/ClientApp/src/app/app.module.ts
+++ RMPlatform/ClientApp/src/app/app.module.ts
@@ -3,15 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    FormComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
