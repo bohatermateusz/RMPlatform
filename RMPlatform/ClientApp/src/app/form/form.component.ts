f5f602bc74435b300224f4821972f7574c459b15
bohatermateusz <bohatermateusz@gmail.com>
2020-06-04 09:53:27 +0200

    Adding necessary changes in form component

--- RMPlatform/ClientApp/src/app/form/form.component.ts
+++ RMPlatform/ClientApp/src/app/form/form.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -9,14 +7,15 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
   styleUrls: ['./form.component.scss']
 })
 export class FormComponent implements OnInit {
-  isLinear = true;
+
+  isLinear = false;
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
 
-  constructor(private _formBuilder: FormBuilder) {}
 
+  constructor(private _formBuilder: FormBuilder) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.firstFormGroup = this._formBuilder.group({
       firstCtrl: ['', Validators.required]
     });
