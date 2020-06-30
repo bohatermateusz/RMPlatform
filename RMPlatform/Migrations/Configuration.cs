2ac6353291bc483b97f08d7814028b943321b4e8
bohatermateusz <bohatermateusz@gmail.com>
2020-06-30 13:08:45 +0200

    Migrations results

---
+++ RMPlatform/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace RMPlatform.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<RMPlatform.DAL.RiskDataContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(RMPlatform.DAL.RiskDataContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
