1fd32dfcf75c79f32626fc0a2452d323fdfc5867
bohatermateusz <bohatermateusz@gmail.com>
2020-07-01 11:35:24 +0200

    added GET to retrieve departments from database

--- RMPlatform/Controllers/WeatherForecastController.cs
+++ RMPlatform/Controllers/WeatherForecastController.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+using RMPlatform.Models;
 
 namespace RMPlatform.Controllers
 {
@@ -12,29 +11,45 @@ namespace RMPlatform.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
+        public IEnumerable<Department> Departments { get; set; }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public IEnumerable<Department> Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            Departments = GetDepartments();
+
+            return Departments;
+
+            //var departments = new Department[]
+            //{
+            //    new Department { DepartmentID = 1, Name = "BOR"},
+            //    new Department { DepartmentID = 2, Name = "HR"},
+            //    new Department { DepartmentID = 3, Name = "GIODO"}
+
+            //};
+
+            //context.Departments.AddRange(departments);
+            //context.SaveChanges();
+
+
+            //var rng = new Random();
+            //return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            //{
+            //    Date = DateTime.Now.AddDays(index),
+            //    TemperatureC = rng.Next(-20, 55),
+            //    Summary = Summaries[rng.Next(Summaries.Length)]
+            //})
+            //.ToArray();
+        }
+
+        private IEnumerable<Department> GetDepartments()
+        {
+            using (var context = new DAL.RiskDataContext())
             {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+                Departments = context.Departments.ToList();
+            }
+            return Departments;
         }
     }
 }
+
